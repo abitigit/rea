b1d8906e8653986dff03d537bda1f34965190beb
abitigit <yohannes.tadesse@aastustudent.edu.et>
2025-08-17 05:35:51 +0000

    firts

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    // Hint: Implement routing and modify this file to render correct components as per requirements.
+    <div>
+      Hint (app.js): To ensure everything works correctly, you may need to add routes in
+      app.js to ensure your components are loaded correctly.
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const homeStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '30vh',
+  textAlign: 'center'
+};
+
+const buttonContainerStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '500px',
+  margin: '20px'
+};
+
+const buttonStyle = {
+  textDecoration: 'none',
+  color: '#fff',
+  backgroundColor: '#525252',
+  padding: '10px 20px',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  marginRight: '10px'
+};
+
+function Home() {
+  return (
+    <div data-testid='home-component' style={homeStyle}>
+      <div style={buttonContainerStyle}>
+         {/* Hint: Implement this */}
+          <button data-testid="register-button" style={buttonStyle}></button>
+          <button data-testid="list-button" style={buttonStyle}></button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
