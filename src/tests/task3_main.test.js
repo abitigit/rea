5675d4dcfdc7a0dfd3c25dc3d75f7268469032bf
Yohannes Mesay <johnnymesay@gmail.com>
2025-08-19 18:25:02 +0300

    test fix

--- src/tests/task3_main.test.js
+++ src/tests/task3_main.test.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import CandidateList from '../components/CandidateList'
+import CandidateList from '../components/CandidateList';
 
 describe('Candidate List Component', () => {
-    it('candidate list component renders without crashing', () => {
-      render(
-        <MemoryRouter>
-          <CandidateList />
-        </MemoryRouter>
-      );
-    });
-  
-    it('displays the "Search skills" input', () => {
-      render(
-        <MemoryRouter>
-          <CandidateList />
-        </MemoryRouter>
-      );
-  
-      expect(screen.getByPlaceholderText('search skills')).toBeInTheDocument();
-    });
-  
-    it('displays the "List All" button', () => {
-      render(
-        <MemoryRouter>
-          <CandidateList />
-        </MemoryRouter>
-      );
-  
-      expect(screen.getByText('List All')).toBeInTheDocument();
-    });
+  it('candidate list component renders without crashing', () => {
+    render(
+      <MemoryRouter>
+        <CandidateList candidates={[]} />
+      </MemoryRouter>
+    );
+  });
 
+  it('displays the "Search by skill..." input', () => {
+    render(
+      <MemoryRouter>
+        <CandidateList candidates={[]} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByPlaceholderText('Search by skill...')).toBeInTheDocument();
+  });
+
+  it('displays the "List All" button', () => {
+    render(
+      <MemoryRouter>
+        <CandidateList candidates={[]} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('List All')).toBeInTheDocument();
+  });
 });
